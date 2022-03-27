e72947157beb1e8eec1e6fa1b71ab76209fdf2d3
NAHIN-JZS <syednahinhossain@gmail.com>
2022-03-27 13:51:13 +0600

    some bug detected in finding nearest k_points. not fixed. agee biriyani kheye ashi

--- KNN_with_KD_tree.py
+++ KNN_with_KD_tree.py
@@ -12,7 +12,7 @@ import math
 #root_node = None
 
 class Node:
-    def __init__(self,x_val,y_val,class_value=0,parent=None):
+    def __init__(self,x_val,y_val,class_value=None,parent=None):
         self.root = parent
         self.ls = [x_val,y_val]
         self.class_value = class_value
@@ -31,17 +31,20 @@ class KNN:
         self.k_nearest_points = []
         
         
-            
+   #------------------model training ended---------------         
         
     def create_KD_tree(self,root,depth, x_value = None, y_value = None,class_value=None, dimention = 2):
+        
+        
         newNode = Node(parent=root,x_val= x_value,y_val= y_value,class_value=class_value)
         
-        
+        #Check if there exixts any node or not. if not then assign the new node as root node
         if ( self.root_node == None):
             self.root_node = newNode
-        if(root == None):
-            
-            return newNode
+        #if(root == None):
+            #return newNode 
+        
+        #as there is node then add it
         else:
             root = self.root_node
             
@@ -49,10 +52,10 @@ class KNN:
             i=0
             while(1): 
                 current_dimention = i % dimention
-                print("depth: ",i)
-                print("dim: ",current_dimention)
-                print(root.ls[current_dimention])
-                print(newNode.ls[current_dimention])
+                #print("depth: ",i)
+                #print("dim: ",current_dimention)
+                #print(root.ls[current_dimention])
+                #print(newNode.ls[current_dimention])
                 i=i+1
                 if(root.ls[current_dimention] > newNode.ls[current_dimention]):
                     if(root.left == None):    
@@ -68,12 +71,16 @@ class KNN:
                         root = root.right
                 
                 #break    
+        #print("KD tree created")
         return self.root_node
     
     def fit(self,data_points):
         #total_points = len(data_points)
         for x,y,z in data_points:
             self.KD_tree = self.create_KD_tree(self.KD_tree,0,x,y,z)
+        print("Model fitted")
+
+#----------------------model training portion ended-----------------------------
 
     def euclidien_distance(self,node1,node2):
         x1=int(node1.ls[0])
@@ -86,15 +93,22 @@ class KNN:
         print(dis, node1.ls)
         return dis
 
+
+
+#------------
+
+
     def nearest_point_detect(self,valueNode):
         nearest_point = self.euclidien_distance(self.root_node, valueNode)
         print(nearest_point)
         root = self.root_node
         while(1):
             temp_root = root
+            if(root == None):
+                break
             if(root.left != None):
                 left_distance = self.euclidien_distance(root.left, valueNode)
-                if(left_distance <= nearest_point):
+                if(left_distance < nearest_point):
                     nearest_point = left_distance
                     temp_root = root.left
                     
@@ -109,7 +123,9 @@ class KNN:
             if(temp_root == root):
                 #if(temp_root in self.nearest_points): #checking if it is already taken or not
                 if(left_distance < right_distance):
+                    #print(root.ls)
                     root = root.left
+                    #print(root.ls)
                     self.k_nearest_points.append([left_distance,root])
                     #heapq.heappush(self.nearest_points, (nearest_point,root))
                     continue
@@ -125,7 +141,6 @@ class KNN:
             root = temp_root
             self.k_nearest_points.append([nearest_point,root])
             
-                
             if(root.left == None and root.right == None):
                 break
         #print(root.ls)
@@ -139,24 +154,28 @@ class KNN:
         print("predicted class: ",predicted_class)
         
     def k_points(self):
-        #kk= len(self.k_nearest_points)
-        kk = self.k
+        
+        #this class will give the k nearest 
+        min_kk= len(self.k_nearest_points)
+        print(min_kk)
+        #kk = self.k
+        #kk=min(min_kk,kk)
         class_counter = [0] * self.no_of_class
-        for i in range(0,kk):
+        for i in range(0,min_kk):
             print(i)
             dis = self.k_nearest_points[i][0]
-            node = self.k_nearest_points[i][1]
-            class_counter[node.class_value] = class_counter[node.class_value] +1
+            nodenew = self.k_nearest_points[i][1]
+            #class_counter[nodenew.class_value] = class_counter[nodenew.class_value] +1
              
             print("Distance : ",dis)
-            print("node : ",node.ls)
-            print("class : ",node.class_value)
+            #print("node : ",nodenew.ls)
+            '''print("class : ",nodenew.class_value)
         
         max_val = max(class_counter)
         selected_class = class_counter.index(max_val) #as has the maximum count
         print("max_count :", max_val)
         
-        return selected_class
+        return selected_class'''
             
     
                     
@@ -164,10 +183,12 @@ class KNN:
         
     
 
-points = [(12,15,0),(5,6,1),(21,24,1),(2,3,1),(13,6,0),(10,9,0),(1,2,0),(14,29,1),(15,10,0)]
-
+#points = [(12,15,1),(5,6,0),(21,24,0),(2,3,0),(13,6,1),(10,9,1),(1,2,1),(14,29,0),(15,10,1)]
+points = [(2,3,1),(5,4,0),(9,6,1),(4,7,1),(8,1,1),(7,2,0)]
 model = KNN(k=3,no_of_class=2)
 model.fit(points)
+node3 = Node(4,3)
+model.predict(node3)
 
 #euclidien_distance((12,15), ())
         
